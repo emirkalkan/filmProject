efa8c3aaafbfd922420ecb6a6b118eb214e6b138
Emir Kalkan <emir.kalkan@huawei.com>
2022-06-05 15:26:18 +0300

    Initial Commit

---
+++ filmProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  filmProject
+//
+//  Created by Emir Kalkan on 5.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
